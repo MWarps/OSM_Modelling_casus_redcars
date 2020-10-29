4b199c0f3c5097a6b0e67f7f3c32bc83907d0ef5
Merlijn <merlijn@warps.nu>
2020-10-29 12:11:30 +0100

    code

---
+++ redcars_workspace/auto/Klant.cpp
@@ -0,0 +1,18 @@
+/*
+ * Klant.cpp
+ *
+ *  Created on: 29 Oct 2020
+ *      Author: yodas
+ */
+
+#include "Klant.h"
+
+Klant::Klant() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Klant::~Klant() {
+	// TODO Auto-generated destructor stub
+}
+
